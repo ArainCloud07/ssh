4efab6befc4412911eb2d34cbc3b6fe1547a3755
Arain Cloud <fluidnodes1508@gmail.com>
2026-01-21 18:03:53 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -95,8 +95,8 @@ printf "  ${YELLOW}Uptime       :${RESET} %s\n\n" "$UPTIME"
 echo -e "${CYAN}Support: support@galaxyhost.site${RESET}"
 echo -e "Website: ${BLUE}galaxyhost.site${RESET}"
 echo -e "${GREEN}Power • Performance • Stability 💪${RESET}"
-echo -e "${YELLOW}\e[1m👑 Respected Founders 👑${RESET}"
-echo -e "${GREEN}\e[1m   Smarty & Shubham${RESET}"
+echo -e ""
+echo -e "${YELLOW}\e[1mRespected Founders:${RESET} ${GREEN}\e[1mSmarty & Shubham${RESET}"
 EOF
 
 chmod +x /etc/update-motd.d/00-galaxyhost
